a1175bf28da658402960f793ca172dca99f49c6e
YuqiHuai <yhuai@uci.edu>
2022-07-25 21:45:34 -0700

    fix: connect to web socket only when sending information

--- apollo/Dreamview.py
+++ apollo/Dreamview.py
@@ -7,32 +7,47 @@ import logging
 class Dreamview:
     def __init__(self, ip: str, port: int) -> None:
         self.url = f"ws://{ip}:{port}/websocket"
-        self.ws = create_connection(self.url)
+        # self.ws = create_connection(self.url)
+        # self.ws.close()
 
-    def reconnect(self):
-        '''
-        Closes the websocket connection and re-creates it so that data can be received again
-        '''
-        self.ws.close()
-        self.ws = create_connection(self.url)
+    # def reconnect(self):
+    #     '''
+    #     Closes the websocket connection and re-creates it so that data can be received again
+    #     '''
+    #     self.ws.close()
+    #     self.ws = create_connection(self.url)
+
+    def send_data(self, data: dict):
+        ws = create_connection(self.url)
+        ws.send(json.dumps(data))
+        ws.close()
 
     def start_sim_control(self):
-        self.ws.send(
-            json.dumps({
-                "type": "StartSimControl"
-            })
-        )
+        # self.ws.send(
+        #     json.dumps({
+        #         "type": "StartSimControl"
+        #     })
+        # )
+        self.send_data({
+            "type": "StartSimControl"
+        })
 
     def stop_sim_control(self):
-        self.ws.send(
-            json.dumps({
-                "type": "StopSimControl"
-            })
-        )
+        # self.ws.send(
+        #     json.dumps({
+        #         "type": "StopSimControl"
+        #     })
+        # )
+        self.send_data({
+            "type": "StopSimControl"
+        })
 
     def reset(self):
-        self.ws.send(
-            json.dumps({
-                "type": "Reset"
-            })
-        )
+        # self.ws.send(
+        #     json.dumps({
+        #         "type": "Reset"
+        #     })
+        # )
+        self.send_data({
+            "type": "Reset"
+        })
