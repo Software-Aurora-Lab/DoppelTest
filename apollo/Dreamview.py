1c76fc9f02a31c5858a4df3766d10c84570236bd
YuqiHuai <yhuai@uci.edu>
2022-07-27 02:23:57 -0700

    Update Dreamview.py
    removed unnecessary comments

--- apollo/Dreamview.py
+++ apollo/Dreamview.py
@@ -7,15 +7,6 @@ import logging
 class Dreamview:
     def __init__(self, ip: str, port: int) -> None:
         self.url = f"ws://{ip}:{port}/websocket"
-        # self.ws = create_connection(self.url)
-        # self.ws.close()
-
-    # def reconnect(self):
-    #     '''
-    #     Closes the websocket connection and re-creates it so that data can be received again
-    #     '''
-    #     self.ws.close()
-    #     self.ws = create_connection(self.url)
 
     def send_data(self, data: dict):
         ws = create_connection(self.url)
@@ -23,31 +14,16 @@ class Dreamview:
         ws.close()
 
     def start_sim_control(self):
-        # self.ws.send(
-        #     json.dumps({
-        #         "type": "StartSimControl"
-        #     })
-        # )
         self.send_data({
             "type": "StartSimControl"
         })
 
     def stop_sim_control(self):
-        # self.ws.send(
-        #     json.dumps({
-        #         "type": "StopSimControl"
-        #     })
-        # )
         self.send_data({
             "type": "StopSimControl"
         })
 
     def reset(self):
-        # self.ws.send(
-        #     json.dumps({
-        #         "type": "Reset"
-        #     })
-        # )
         self.send_data({
             "type": "Reset"
         })
