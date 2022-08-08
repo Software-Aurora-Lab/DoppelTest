4c511c035320af0038076bd1df328c924bd10e2e
YuqiHuai <yhuai@uci.edu>
2022-08-07 23:06:08 -0700

    added: force invalid traffic signal

--- scenario/TrafficManager.py
+++ scenario/TrafficManager.py
@@ -2,6 +2,8 @@ from time import time
 from automation.section_tc import TCSection
 from modules.perception.proto.traffic_light_detection_pb2 import TrafficLightDetection, TrafficLight
 
+from utils.config import FORCE_INVALID_TRAFFIC_CONTROL
+
 
 class TrafficControlManager:
     def __init__(self, tc: TCSection) -> None:
@@ -9,19 +11,23 @@ class TrafficControlManager:
         self.sequence_num = 0
 
     def get_traffic_configuration(self, curr_t: float):
-        if curr_t <= self.tc.duration_g:
-            # green duration
-            config = self.tc.initial
-            pass
-        elif curr_t <= self.tc.duration_g + self.tc.duration_y:
-            config = self.tc.calculate_transition()
-            # yellow duration
-            pass
-        elif curr_t <= self.tc.duration_g + self.tc.duration_y + self.tc.duration_b:
-            # buffer duration
-            config = self.tc.get_all('RED')
+
+        if FORCE_INVALID_TRAFFIC_CONTROL:
+            config = self.tc.get_all('GREEN')
         else:
-            config = self.tc.final
+            if curr_t <= self.tc.duration_g:
+                # green duration
+                config = self.tc.initial
+                pass
+            elif curr_t <= self.tc.duration_g + self.tc.duration_y:
+                config = self.tc.calculate_transition()
+                # yellow duration
+                pass
+            elif curr_t <= self.tc.duration_g + self.tc.duration_y + self.tc.duration_b:
+                # buffer duration
+                config = self.tc.get_all('RED')
+            else:
+                config = self.tc.final
 
         tld = TrafficLightDetection()
         tld.header.timestamp_sec = time()
