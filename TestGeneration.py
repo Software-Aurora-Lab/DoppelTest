2c204e84061ce6685d6ed8357d0950213ef7b944
YuqiHuai <yhuai@uci.edu>
2022-08-07 23:08:42 -0700

    Update TestGeneration.py

--- TestGeneration.py
+++ TestGeneration.py
@@ -1,4 +1,3 @@
-
 from modules.map.proto.map_pb2 import Map
 from apollo.ApolloContainer import ApolloContainer
 from automation.Chromosome import Chromosome
@@ -7,6 +6,7 @@ from scenario.ChromosomeRunner import ChromosomeRunner
 from utils import make_dir_for_generation
 from utils.config import APOLLO_ROOT
 from uuid import uuid4
+
 map = Map()
 f = open('./data/maps/borregas_ave_fix/base_map.bin', 'rb')
 map.ParseFromString(f.read())
@@ -24,15 +24,11 @@ for ctn in containers:
 make_dir_for_generation('TestGeneration')
 
 for chromosome in pop:
-
     runner = ChromosomeRunner(map, containers)
-    counter = 0
-    while True:
-        print(counter)
-        print([x.start_time for x in chromosome.AD.adcs])
-        runner.set_chromosome(chromosome)
-        runner.init_scenario()
-        runner.run_scenario(generation_name='TestGeneration', run_id=f'{str(uuid4())}',
-                            upper_limit=55, save_record=True)
-        counter += 1
-        break
+    runner.set_chromosome(chromosome)
+    runner.init_scenario()
+    runner.run_scenario(
+        generation_name='TestGeneration',
+        run_id=f'{str(uuid4())}',
+        upper_limit=55, save_record=True
+    )
