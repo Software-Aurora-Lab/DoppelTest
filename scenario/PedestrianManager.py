9b1483dca19c2128ae85e24bdf8a2295763308f3
YuqiHuai <yhuai@uci.edu>
2022-08-11 14:29:28 -0700

    Update PedestrianManager.py

--- scenario/PedestrianManager.py
+++ scenario/PedestrianManager.py
@@ -1,31 +1,17 @@
-from dataclasses import dataclass
 import math
 from typing import List, Tuple
-
+from automation.section_pd import PD, PDSection
+from modules.common.proto.geometry_pb2 import Point3D
 from shapely.geometry import MultiLineString, LineString
 
 
-@dataclass
-class Point3D:
-    x: int = 0
-    y: int = 0
-    z: int = 0
-
-
-@dataclass
-class Pedestrian:
-    boundary: List[Point3D]
-    speed: float
-    start_time: float
-
-
 class PedestrianManager:
-    pedestrians: List[Pedestrian]
+    pedestrians: PDSection
 
-    def __init__(self, pedestrians: List[Pedestrian]) -> None:
+    def __init__(self, pedestrians: PDSection) -> None:
         self.pedestrians = pedestrians
 
-    def calculate_position(self, pd: Pedestrian, time_spent_walking: float) -> Tuple[Point3D, float]:
+    def calculate_position(self, pd: PD, time_spent_walking: float) -> Tuple[Point3D, float]:
         '''
         Return the pedestrians location and heading
         '''
@@ -59,7 +45,7 @@ class PedestrianManager:
 
     def get_pedestrians(self, curr_time: float) -> List[Tuple[Point3D, float]]:
         result = list()
-        for pd in self.pedestrians:
+        for pd in self.pedestrians.pds:
             time_spent_walking = curr_time - pd.start_time
             if time_spent_walking > 0:
                 pd_position = self.calculate_position(pd, time_spent_walking)
@@ -68,22 +54,3 @@ class PedestrianManager:
                     pd.boundary[1].y-pd.boundary[0].y, pd.boundary[1].x-pd.boundary[0].x)
             result.append(pd_position)
         return result
-
-
-pd = Pedestrian(
-    boundary=[Point3D(0, 0),
-              Point3D(10, 0),
-              Point3D(10, 10),
-              Point3D(0, 10)],
-    speed=1,
-    start_time=5
-)
-
-pm = PedestrianManager([pd])
-
-print(pm.get_pedestrians(0))
-# [(Point3D(x=0, y=0, z=0), 0.0)] Pedestrian hasn't moved
-print(pm.get_pedestrians(4))
-# [(Point3D(x=0, y=0, z=0), 0.0)] again
-print(pm.get_pedestrians(6))
-# [(Point3D(x=1, y=0, z=0), 0.0)] # expected output, pedestrian is given 1 second to travel 1 meter around the boundary
