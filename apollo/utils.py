8fb11b1636267c904b9ba19f00d954df925a76b0
YuqiHuai <yhuai@uci.edu>
2022-08-10 21:17:27 -0700

    Update utils.py

--- apollo/utils.py
+++ apollo/utils.py
@@ -4,6 +4,9 @@ from modules.localization.proto.localization_pb2 import LocalizationEstimate
 from modules.perception.proto.perception_obstacle_pb2 import PerceptionObstacle
 from config import APOLLO_VEHICLE_HEIGHT, APOLLO_VEHICLE_LENGTH, APOLLO_VEHICLE_WIDTH, APOLLO_VEHICLE_back_edge_to_center
 from dataclasses import dataclass
+from shapely.geometry import Polygon
+from typing import Set, Tuple
+from modules.planning.proto.planning_pb2 import ADCTrajectory
 
 
 @dataclass
@@ -92,6 +95,10 @@ def generate_adc_polygon(position: Point3D, theta: float):
     return points
 
 
+def obstacle_to_polygon(obs: PerceptionObstacle) -> Polygon:
+    return Polygon([[p.x, p.y] for p in obs.polygon_point])
+
+
 def localization_to_obstacle(_id: int, data: LocalizationEstimate) -> PerceptionObstacle:
     """
     Converts LocalizationEstimate to PerceptionObstacle
@@ -126,3 +133,51 @@ def localization_to_obstacle(_id: int, data: LocalizationEstimate) -> Perception
             position, data.pose.heading)
     )
     return obs
+
+
+def extract_main_decision(data: ADCTrajectory) -> Set[Tuple]:
+    main_decision = data.decision.main_decision
+    object_decisions = data.decision.object_decision.decision
+
+    decisions = set()
+
+    # analyze main decision
+    if main_decision.HasField('cruise'):
+        # FORWARD = 0, LEFT = 1, RIGHT = 2
+        md = ('main', 'cruise', main_decision.cruise.change_lane_type)
+    elif main_decision.HasField('stop'):
+        # modules/planning/proto/decision.proto:19
+        md = ('main', 'stop', main_decision.stop.reason_code,
+              main_decision.stop.reason)
+    elif main_decision.HasField('estop'):
+        # modules/planning/proto/decision.proto:150
+        md = ('main', 'estop', main_decision.estop.reason_code)
+    elif main_decision.HasField('mission_complete'):
+        md = ('main', 'mission_complete',)
+    else:
+        md = ('main', 'not_ready',)
+
+    decisions.add(md)
+
+    # analyze object decision
+    for obj_d in object_decisions:
+        object_decision = obj_d.object_decision[0]
+        if object_decision.HasField('stop'):
+            od = ('obj', 'stop', object_decision.stop.reason_code)
+        elif object_decision.HasField('follow'):
+            od = ('obj', 'follow')
+        elif object_decision.HasField('yield'):
+            od = ('obj', 'yield')
+        elif object_decision.HasField('overtake'):
+            od = ('obj', 'overtake')
+        elif object_decision.HasField('nudge'):
+            od = ('obj', 'nudge', object_decision.nudge.type)
+        elif object_decision.HasField('avoid'):
+            od = ('obj', 'avoid')
+        elif object_decision.HasField('side_pass'):
+            od = ('obj', 'side_pass', object_decision.side_pass.type)
+        else:  # object_decision.HasField('ignore')
+            od = ('obj', 'ignore')
+        decisions.add(od)
+
+    return decisions
