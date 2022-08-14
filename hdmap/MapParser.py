6ec729dbaf7016c776a704c4b76440b133371a9e
YuqiHuai <yhuai@uci.edu>
2022-08-14 00:16:21 -0700

    Update MapParser.py

--- hdmap/MapParser.py
+++ hdmap/MapParser.py
@@ -1,9 +1,9 @@
 from collections import defaultdict
-from typing import List, Tuple
+from typing import List, Set, Tuple
 from hdmap import load_hd_map
 from modules.map.proto.map_pb2 import Map
 import networkx as nx
-from shapely.geometry import LineString, MultiLineString
+from shapely.geometry import LineString, Point
 from modules.common.proto.geometry_pb2 import PointENU
 import math
 
@@ -20,6 +20,7 @@ class MapParser:
     __lanes_controlled_by_signal: dict
 
     __signal_relations: nx.Graph
+    __lane_nx: nx.DiGraph
 
     def __init__(self, filename: str) -> None:
         self.__map = load_hd_map(filename)
@@ -29,6 +30,7 @@ class MapParser:
         self.load_crosswalks()
         self.parse_relations()
         self.parse_signal_relations()
+        self.parse_lane_relations()
 
     def load_junctions(self):
         self.__junctions = dict()
@@ -75,11 +77,6 @@ class MapParser:
                     if self.__is_overlap(self.__signals[sid], self.__lanes[lid]):
                         self.__lanes_controlled_by_signal[sid].append(lid)
 
-    def __is_overlap(self, obj1, obj2):
-        oid1 = set([x.id for x in obj1.overlap_id])
-        oid2 = set([x.id for x in obj2.overlap_id])
-        return oid1 & oid2 != set()
-
     def parse_signal_relations(self):
         g = nx.Graph()
         for junk, junv in self.__junctions.items():
@@ -96,6 +93,29 @@ class MapParser:
                     elif self.is_conflict_lanes(lg1, lg2):
                         g.add_edge(sid1, sid2, v='NE')
         self.__signal_relations = g
+
+    def parse_lane_relations(self):
+        dg = nx.DiGraph()
+        for lane1 in self.__lanes:
+            dg.add_node(lane1)
+            for lane2 in self.__lanes:
+                if lane1 == lane2:
+                    continue
+                line1 = self.get_lane_central_curve(lane1)
+                line2 = self.get_lane_central_curve(lane2)
+                s1, e1 = Point(line1.coords[0]), Point(line1.coords[-1])
+                s2, e2 = Point(line2.coords[0]), Point(line2.coords[-1])
+
+                if s1.distance(e2) < 0.001:
+                    dg.add_edge(lane2, lane1)
+                elif e1.distance(s2) < 0.001:
+                    dg.add_edge(lane1, lane2)
+        self.__lane_nx = dg
+
+    def __is_overlap(self, obj1, obj2):
+        oid1 = set([x.id for x in obj1.overlap_id])
+        oid2 = set([x.id for x in obj2.overlap_id])
+        return oid1 & oid2 != set()
 
     def get_signals_wrt(self, signal_id: str) -> Tuple[str, str]:
         result = list()
@@ -124,13 +144,47 @@ class MapParser:
         return self.get_lane_central_curve(lane_id).length
 
     def get_coordinate_and_heading(self, lane_id: str, s: float):
-        mtl = self.get_lane_central_curve(lane_id)
-        ip = mtl.interpolate(s)
+        lst = self.get_lane_central_curve(lane_id)
+        ip = lst.interpolate(s)
 
-        segments = list(map(LineString, zip(mtl.coords[:-1], mtl.coords[1:])))
+        segments = list(map(LineString, zip(lst.coords[:-1], lst.coords[1:])))
         segments.sort(key=lambda x: ip.distance(x))
         line = segments[0]
         x1, x2 = line.xy[0]
         y1, y2 = line.xy[1]
 
         return (PointENU(x=ip.x, y=ip.y), math.atan2(y2-y1, x2-x1))
+
+    def get_junctions(self) -> List[str]:
+        return list(self.__junctions.keys())
+
+    def get_lanes(self) -> List[str]:
+        return list(self.__lanes.keys())
+
+    def get_crosswalks(self) -> List[str]:
+        return list(self.__crosswalk.keys())
+
+    def get_signals(self) -> List[str]:
+        return list(self.__signals.keys())
+
+    def get_lanes_not_in_junction(self) -> Set[str]:
+        lanes = set(self.get_lanes())
+        for junc in self.__lanes_at_junction:
+            jlanes = set(self.__lanes_at_junction[junc])
+            lanes = lanes - jlanes
+        return lanes
+
+    def get_path_from(self, lane_id: str) -> List[List[str]]:
+        target_lanes = self.get_lanes_not_in_junction()
+        reachable = self.__get_reachable_from(lane_id)
+        return [p for p in reachable if p[-1] in target_lanes]
+
+    def __get_reachable_from(self, lane_id: str, depth=8):
+        if depth == 1:
+            return [[lane_id]]
+        result = list()
+        for u, v in self.__lane_nx.edges(lane_id):
+            result.append([u, v])
+            for rp in self.__get_reachable_from(v, depth-1):
+                result.append([u] + rp)
+        return result
