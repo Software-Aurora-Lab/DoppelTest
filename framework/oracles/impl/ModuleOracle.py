f95bc4b8415038f8fb76f726eb923aa3e070f3af
YuqiHuai <yhuai@uci.edu>
2022-08-26 10:57:42 -0700

    fix: routing oracle should check for routing_response

--- framework/oracles/impl/ModuleOracle.py
+++ framework/oracles/impl/ModuleOracle.py
@@ -14,30 +14,20 @@ class ModuleOracle(OracleInterface):
         self.prev_ = None
         self.next_ = None
         self.distance_traveled = -1.0
+        self.received_routing = False
 
     def get_interested_topics(self):
         return [
-            '/apollo/localization/pose',
+            '/apollo/routing_response',
         ]
 
     def on_new_message(self, topic: str, message, t):
-        if self.prev_ is None and self.next_ is None:
-            self.prev_ = message
-            self.distance_traveled = 0.0
-            return
-        self.next_ = message
-        # analyze
-        prev_point = Point(self.prev_.pose.position.x,
-                           self.prev_.pose.position.y)
-        next_point = Point(self.next_.pose.position.x,
-                           self.next_.pose.position.y)
-        self.distance_traveled += next_point.distance(prev_point)
-        # update prev_
-        self.prev_ = message
+        if topic == '/apollo/routing_response':
+            self.received_routing = True
 
     def get_result(self):
         result = list()
-        if self.distance_traveled == 0.0:
+        if not self.received_routing:
             result.append(('module', 'routing failure'))
         # elif self.distance_traveled == -1.0:
         #     result.append(('module', 'sim control failure'))
