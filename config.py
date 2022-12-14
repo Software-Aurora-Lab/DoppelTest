f74af271734f66c562b60d33301f0f2262db321a
YuqiHuai <yhuai@uci.edu>
2022-12-14 13:27:26 -0800

    Update variable names and comments

--- config.py
+++ config.py
@@ -7,27 +7,27 @@ import logging
 
 
 # APOLLO CONFIGURATION ==============================
-PERCEPTION_FREQUENCY = 25
+PERCEPTION_FREQUENCY                = 25
 
-APOLLO_VEHICLE_LENGTH = 4.933
-APOLLO_VEHICLE_WIDTH = 2.11
-APOLLO_VEHICLE_HEIGHT = 1.48
-APOLLO_VEHICLE_back_edge_to_center = 1.043
+APOLLO_VEHICLE_LENGTH               = 4.933
+APOLLO_VEHICLE_WIDTH                = 2.11
+APOLLO_VEHICLE_HEIGHT               = 1.48
+APOLLO_VEHICLE_back_edge_to_center  = 1.043
 
 
 # DIRECTORIES =======================================
-APOLLO_ROOT = '/home/sora/Desktop/yhuai/apollo'
-MAGGIE_ROOT = '/home/sora/Desktop/yhuai/DoppelTest'
-RECORDS_DIR = f'{MAGGIE_ROOT}/data/records'
-LOG_DIR = f'{MAGGIE_ROOT}/data/Logs'
+APOLLO_ROOT = '/home/yuqi/ResearchWorkspace/Baidu Apollo/DoppelTest_Apollo'
+DT_ROOT     = '/home/yuqi/ResearchWorkspace/DoppelTest/tool'
+RECORDS_DIR = f'{DT_ROOT}/data/records'
+LOG_DIR     = f'{DT_ROOT}/data/Logs'
 
-# MAGGIE CONFIGS ====================================
-STREAM_LOGGING_LEVEL = logging.INFO
-USE_SIM_CONTROL_STANDALONE = True
-FORCE_INVALID_TRAFFIC_CONTROL = False
-SCENARIO_UPPER_LIMIT = 30
-INSTANCE_MAX_WAIT_TIME = 15
-MAX_ADC_COUNT = 5
-MAX_PD_COUNT = 5
-RUN_FOR_HOUR = 12
-HD_MAP_PATH = f'{MAGGIE_ROOT}/data/maps/shalun/base_map.bin'
+# DoppelTest CONFIGS ====================================
+STREAM_LOGGING_LEVEL            = logging.INFO
+USE_SIM_CONTROL_STANDALONE      = True
+FORCE_INVALID_TRAFFIC_CONTROL   = False
+SCENARIO_UPPER_LIMIT            = 30
+INSTANCE_MAX_WAIT_TIME          = 15
+MAX_ADC_COUNT                   = 5
+MAX_PD_COUNT                    = 5
+RUN_FOR_HOUR                    = 12
+HD_MAP_PATH                     = f'{DT_ROOT}/data/maps/san_mateo/base_map.bin'
