772855a70e3540f489ec388177f3a217bc9fb9b9
Y <yhuai@uci.edu>
2023-01-23 00:01:03 -0800

    Delete test_rerun.py

--- test_rerun.py
+++
@@ -1,8 +0,0 @@
-from config import HD_MAP_PATH
-from framework.scenario import Scenario
-from hdmap.MapParser import MapParser
-
-ma = MapParser(HD_MAP_PATH)
-chromosome = Scenario.from_json('/home/sora/Desktop/yhuai/DoppelTest/data/c.json')
-
-print(chromosome)
