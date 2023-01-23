4a8122ab194d66a9a5a3313aa3ea172a322952f7
Y <yhuai@uci.edu>
2023-01-23 01:19:06 -0800

    update extension autosectionlabel

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -58,3 +59,4 @@ html_static_path = [
 ]
 
 master_doc = 'index'
+autosectionlabel_prefix_document = True
