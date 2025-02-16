c3f6fcded20b6ac0ae16a553b1151614e3de4388
Yuqi Huai <yhuai@uci.edu>
2025-02-16 13:03:37 -0800

    chore(root): update conda script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,6 @@
 ENV_NAME="doppeltest"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PARENT_DIRECTORY=$(dirname "$CURRENT_DIR")
 
 # exit if conda is not installed
 if ! command -v conda &> /dev/null
@@ -21,6 +23,6 @@ fi
 
 # install decictor dependencies
 echo "Installing dependencies"
-$PIP install -r requirements.txt
+$PIP install -r $PARENT_DIRECTORY/requirements.txt
 
 echo -e "\033[0;32mConda environment $ENV_NAME is now ready.\033[0m"
