fc75631ac8808d6d63ea3f817b7b4aee0a966551
Yuqi Huai <yhuai@uci.edu>
2025-02-16 13:00:20 -0800

    chore(root): replace poetry with conda

---
+++ scripts/setup.sh
@@ -0,0 +1,26 @@
+ENV_NAME="doppeltest"
+
+# exit if conda is not installed
+if ! command -v conda &> /dev/null
+then
+    echo "Conda is not installed. Exiting..."
+    exit 1
+fi
+
+BASE_DIR=$(conda info --base)
+PIP="$BASE_DIR/envs/$ENV_NAME/bin/pip"
+
+# create conda env decictor if not exist
+if ! conda info --envs | grep -q "$ENV_NAME"
+then
+    echo "Creating conda enviroment $ENV_NAME"
+    conda create -n $ENV_NAME python=3.8.10 -y
+else
+    echo "Conda envviroment $ENV_NAME exists"
+fi
+
+# install decictor dependencies
+echo "Installing dependencies"
+$PIP install -r requirements.txt
+
+echo -e "\033[0;32mConda environment $ENV_NAME is now ready.\033[0m"
